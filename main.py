585aefc9c10cc5a4b1d2a06508cc09e78d89161f
Martica <58595345+Martica199005@users.noreply.github.com>
2020-03-21 19:30:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 import os
 from twilio.rest import Client
 
-
-password=os.getenv("USER")
-user=os.getenv("PASSWORD")
 account_sid=os.getenv("ACCOUNT_SID")
 token=os.getenv("TOKEN")
 my_cell=os.getenv("MY_CELL")
 my_twiliocell=os.getenv("MY_TWILIOCELL")
 
-# env test
-
-if user and password:
-  print("Username and password are:\n"+user+"\n"+password)
-else:
-  print("Your are not the owner")
 
 def sendSMS(msg):
   try:
